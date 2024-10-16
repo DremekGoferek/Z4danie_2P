2a7d5afd939c74f868750a0646ffa0ce89dc1a8d
meyerek1 <gryantek08@gmail.com>
2024-10-16 12:43:07 +0200

    while

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -2,14 +2,12 @@
 {
     static void Main(string[] args)
     {
-        for (int i = 0; i < 10; i++)
-        {
+        int i = 0;
+while (i > 10) {
             Console.WriteLine(i);
+            i++;
+        
         }
-
-
-        {
 
         }
     }
-}
